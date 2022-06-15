e776ede0f5098160d895711f79eab91460caf0f8
Andersooncr <andersondocarmo.970@gmail.com>
2022-06-14 22:12:42 -0300

    feat:redirecionamento para o wpp

--- src/script/cotacao.js
+++ src/script/cotacao.js
@@ -1,34 +1,38 @@
-const select = document.querySelector("#valor")
-const checkBoxes = document.querySelectorAll(".checkbox")
-const model = document.querySelector("#model")
-const btn = document.querySelector("#send")
+const select = document.querySelector("#valor");
+const checkBoxes = document.querySelectorAll(".checkbox");
+const model = document.querySelector("#model");
+const btn = document.querySelector("#send");
 
-function actualizedSelect(){
-    const optionValue = select.options[select.selectedIndex]
-    return optionValue.value
+function actualizedSelect() {
+  const { value } = select.options[select.selectedIndex];
+  return value;
 }
 
-function checkBox(){
-    let selected = 0
-    checkBoxes.forEach(function(el){
-        if(el.checked) {
-            selected++
-        }
-    })
-    return selected 
-}
-
-btn.addEventListener("click", function(e) {
-    e.preventDefault()
-    if(model.value){
-        const valueFipe = actualizedSelect()
-        const additional = checkBox()
-        const calculate = (valueFipe === 1 ? 70 : 70 + valueFipe*10) + additional * 5
-    
-        document.getElementById('valorfinal').innerHTML = `O valor da mensalidade é de R$ ${calculate},00. \n Para finalizar a contratação entre em contato com +55 31 9999-8838`
-        return calculate
-    }else{
-        document.getElementById('valorfinal').innerHTML = `Você se esqueceu de preencher um campo`
+function checkBox() {
+  let selected = 0;
+  checkBoxes.forEach(function (el) {
+    if (el.checked) {
+      selected++;
     }
-    
-})
+  });
+  return selected;
+}
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (model.value) {
+    const valueFipe = actualizedSelect();
+    const additional = checkBox();
+    const calculate =
+      (valueFipe === 1 ? 70 : 70 + valueFipe * 10) + additional * 5;
+
+    const redirect = confirm(
+      `O valor da mensalidade é de R$ ${calculate},00. \nCaso queira continuar a contratação via whatsapp clique no OK`
+    );
+    if(redirect){
+        location.href='https://contate.me/vprotectionoficial1'
+    }
+  } else {
+    alert(`Você se esqueceu de preencher um campo`);
+  }
+});
